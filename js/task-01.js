12e6bdf1c59210740f4b00fbe7ac84fa10cc5a4d
kesel <a.kiselyoff@gmail.com>
2022-01-31 22:07:55 +0200

    task-01-done

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesItems = document.querySelectorAll('.item');
+console.log(categoriesItems);
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(item => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
