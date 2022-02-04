ed0c269807a61cebae743113015ab1707fcfeacf
kesel <a.kiselyoff@gmail.com>
2022-02-05 01:21:45 +0200

    task-09-done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const currentColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+btnChangeColor.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = getRandomHexColor();
+}
