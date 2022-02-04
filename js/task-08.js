18be1a39c6ae2d367d0c3da9f5df351b15d92497
kesel <a.kiselyoff@gmail.com>
2022-02-05 00:57:30 +0200

    task-08-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.dir(event.currentTarget);
+  if (
+    event.currentTarget.elements.email.value &&
+    event.currentTarget.elements.password.value
+  ) {
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+    // const formData = new FormData(event.currentTarget);  alternative getting form fields
+    // return formData;
+    console.dir({ email, password });
+
+    // return { email, password };
+  } else {
+    alert('все поля должны быть заполнены');
+  }
+
+  form.reset();
+}
